4699d4a818c1573fafd590be8e2102855a74ce30
Priyanka Mukund <priyanka@Pantaloon.local>
2018-12-09 22:25:19 +1100

    created game logic

---
+++ memory game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+var cardOne = "Queen";
+var cardTwo = "King";
+var cardThree = "Diamond";
+var cardFour = "Heart";
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardTwo);
+console.log("User flipped" + cardThree);
+console.log("User flipped" + cardFour);
