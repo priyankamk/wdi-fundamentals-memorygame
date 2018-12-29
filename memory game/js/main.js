e98ddd69155868d613eb61f148f15afd0f945b71
Priyanka Mukund <priyanka@Pantaloon.local>
2018-12-30 00:59:59 +1100

    created game logic

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -1,11 +1,36 @@
 console.log("Up and running!");
 
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "Diamond";
-var cardFour = "Heart";
+// var cardOne = "Queen";
+// var cardTwo = "King";
+// var cardThree = "Diamond";
+// var cardFour = "Heart";
 
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardTwo);
-console.log("User flipped" + cardThree);
-console.log("User flipped" + cardFour);
+// console.log("User flipped" + cardOne);
+// console.log("User flipped" + cardTwo);
+// console.log("User flipped" + cardThree);
+// console.log("User flipped" + cardFour);
+
+// var flipCard = function(cardId){
+
+// }
+
+var cards = ["Queen","Queen","King","King"];
+
+var cardsInPlay = [];
+
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+// cardsInPlay = console.log("User flipped Queen");
+
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+// cardsInPlay = console.log("User flipped King");
+
+if (cardsInPlay[0].length === 2 && cardsInPlay[2].length !== 2) {
+	alert("You found a match");
+} else {
+	alert("Sorry, try again");
+}
+
