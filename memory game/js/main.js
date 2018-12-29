eccd668c8f56a2c4dbcf660e5458df1a1318aaa6
Priyanka Mukund <priyanka@Pantaloon.local>
2018-12-30 02:14:20 +1100

    created game logic

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -10,27 +10,47 @@ console.log("Up and running!");
 // console.log("User flipped" + cardThree);
 // console.log("User flipped" + cardFour);
 
-// var flipCard = function(cardId){
-
-// }
 
 var cards = ["Queen","Queen","King","King"];
 
 var cardsInPlay = [];
 
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+}
+};
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+var flipCard = function(cardId) {
+
+	var cardOne = cards[0];
+	cardsInPlay.push(cardOne);
+	console.log("User flipped" + cardOne);
+
+
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+	console.log("User flipped" + cardTwo);
+
+	console.log("User flipped" + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	// if (cardsInPlay[0].length === 2 && cardsInPlay[2].length !== 2) {
+	// alert("You found a match");
+	// } else {
+	// alert("Sorry, try again");
+};
+// };
+checkForMatch();
+flipCard(0);
+flipCard(2);
+
 // cardsInPlay = console.log("User flipped Queen");
 
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
 // cardsInPlay = console.log("User flipped King");
 
-if (cardsInPlay[0].length === 2 && cardsInPlay[2].length !== 2) {
-	alert("You found a match");
-} else {
-	alert("Sorry, try again");
-}
 
