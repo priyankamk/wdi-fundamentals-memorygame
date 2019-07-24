9e181d3f81c6f5e89b09f92d294d004ff9154ab0
Priyanka Mukund <priyanka@kunday.com>
2019-07-24 12:54:07 +1000

    initial commit.

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -11,12 +11,34 @@ console.log("Up and running!");
 // console.log("User flipped" + cardFour);
 
 
-var cards = ["Queen","Queen","King","King"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
+	}
+];
 
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+	if (cardsInPlay[0] === cardsInPlay[2]) {
 		console.log("You found a match!");
 	} else {
 		console.log("Sorry, try again.");
@@ -27,14 +49,16 @@ var flipCard = function(cardId) {
 
 	var cardOne = cards[0];
 	cardsInPlay.push(cardOne);
-	console.log("User flipped" + cardOne);
+	// console.log("User flipped" + cardOne);
 
 
 	var cardTwo = cards[2];
 	cardsInPlay.push(cardTwo);
-	console.log("User flipped" + cardTwo);
+	// console.log("User flipped" + cardTwo);
 
-	console.log("User flipped" + cards[cardId]);
+	console.log("User flipped" + cards[cardId].rank);
+	console.log("User flipped" + cards[cardId].suit);
+	console.log("User flipped" + cards[cardId].cardImage);
 
 	cardsInPlay.push(cards[cardId]);
 
@@ -45,12 +69,28 @@ var flipCard = function(cardId) {
 };
 // };
 checkForMatch();
-flipCard(0);
-flipCard(2);
+// flipCard(0);
+// flipCard(2);
 
 // cardsInPlay = console.log("User flipped Queen");
 
 
 // cardsInPlay = console.log("User flipped King");
+
+
+var createBoard = function() {
+	for(var i = 0; i < arrayName.length; i++) {
+		var cardElement = document.createElement('img');
+
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+
+		cardElement.appendChild('game-board');
+
+	}
+};
+
+createBoard();
 
 
